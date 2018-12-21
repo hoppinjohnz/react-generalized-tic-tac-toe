e328515f23cd2ed07d53720f895b05f4ce5db698
John Zhu <john.zhu@morgan6.com>
2018-12-21 12:32:10 -0500

    interactive square component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => alert('clicked!')}>
                 {this.props.value}
             </button>
         );
