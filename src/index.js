b57f2a9ec116ff4568310e777d4a2b4beb3a8304
John Zhu <john.zhu@morgan6.com>
2018-12-23 19:59:50 -0500

    3. Rewrite Board to use two loops to make the squares instead of hardcoding them

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var DIM = 3;
+
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
@@ -13,32 +15,42 @@ function Square(props) {
 class Board extends React.Component {
     renderSquare(i) {
         return (
-            <Square 
+            <Square key={i}
                 value={this.props.squares[i]} 
                 onClick={() => this.props.onClick(i)}
             />
         );
     }
 
-    render() {
+    boardRow(rw) {
+        const cols = rw.slice();
+        return (
+            <div key={cols} className="board-row">
+                {cols.map((i) => this.renderSquare(i))}
+            </div>
+        );
+    }
+
+    fullBoard() {
+        const a = Array(DIM).fill(null);
+        for (let i = 0; i < DIM; i++) {
+            const b = Array(DIM).fill(null);
+            for (let j = 0; j < DIM; j++) {
+                b[j] = i * DIM + j;
+            }
+            a[i] = b;
+        }
+        // a = [[0, 1, 2], [3, 4, 5], [6, 7, 8]] for dim = 3;
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {a.map((e) => this.boardRow(e))}
             </div>
+        );
+    }
+
+    render() {
+        return (
+            this.fullBoard()
         );
     }
 }
